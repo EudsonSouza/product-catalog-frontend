ae83d088d4b414061834ea4657a48eb382ff0f54
Eudson Souza <eudson.souza@outlook.com>
2025-11-02 19:14:50 -0300

    refactor: standardize component filenames to kebab-case

--- src/components/features/search/__tests__/search-bar.test.tsx
+++ src/components/features/search/__tests__/search-bar.test.tsx
@@ -1,7 +1,7 @@
 import { describe, it, expect, vi } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import SearchBar from '../SearchBar'
+import SearchBar from '../search-bar'
 
 describe('SearchBar', () => {
   describe('Main Flow - Search Functionality', () => {
