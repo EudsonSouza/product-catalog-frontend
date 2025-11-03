14380c30ce86d12c2cb0d6b904442149d42fde40
Eudson Souza <eudson.souza@outlook.com>
2025-11-02 21:06:02 -0300

    chore(vitest): use thresholds structure for coverage config

--- vitest.config.ts
+++ vitest.config.ts
@@ -28,10 +28,12 @@ export default defineConfig({
       ],
       include: ['src/**/*.{ts,tsx}'],
       all: true,
-      lines: 80,
-      functions: 80,
-      branches: 80,
-      statements: 80,
+      thresholds: {
+        lines: 80,
+        functions: 80,
+        branches: 80,
+        statements: 80,
+      },
     },
   },
   resolve: {
